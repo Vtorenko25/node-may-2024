781b8e3e72791adc85e1ba948f6be0643f574cee
Vtorenko25 <oleg.vtorenko@gmail.com>
2024-12-05 19:43:16 +0200

    HW2

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ const fs = require('node:fs/promises');
 
 const foo = async () => {
     const dirs = ['user', 'users', 'post', 'posts', 'comment'];
+    const files = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
+
     for (const dirName of dirs) {
-        await fs.mkdir(path.join('baseFolder', dirName), { recursive: true });
+        const dirPath = path.join('baseFolder', dirName);
+        await fs.mkdir(dirPath, { recursive: true });
+
+        for (const fileName of files) {
+            const filePath = path.join(dirPath, fileName);
+            await fs.writeFile(filePath, `Це файл ${fileName} у папці ${dirName}`);
+        }
     }
+
 };
 
 void foo();
