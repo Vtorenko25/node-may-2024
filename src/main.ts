91c80c7e52ef0d1d9b020d428c2195921219a59e
Vtorenko25 <oleg.vtorenko@gmail.com>
2024-12-26 11:34:24 +0200

    HW4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
-import { read } from "./fs.service";
+import { ApiError } from "./errors/api-error";
+import { userRouter } from "./routers/user.router";
 
 dotenv.config();
 
@@ -10,97 +11,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/users", async (req: Request, res: Response): Promise<void> => {
-  try {
-    const users = await read();
-    res.json(users);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
+app.use("/users", userRouter);
+
+app.use(
+  "*",
+  (error: ApiError, req: Request, res: Response, next: NextFunction) => {
+    const status = error.status || 500;
+    const message = error.message ?? "Something went wrong";
+
+    res.status(status).json({ status, message });
+  },
+);
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  process.exit(1);
 });
 
-// app.post('/users', async (req:Request, res:Response):Promise<void> => {
-//     try {
-//         if (!req.body.name || req.body.name.length < 3) {
-//             return res.status(400).json('Name is required and should be minimum 3 symbols');
-//         }
-//         if (!req.body.email || !req.body.email.includes('@') ) {
-//             return res.status(400).json('Email is required');
-//         }
-//         if (!req.body.password || req.body.password.length < 8) {
-//             return res.status(400).json('Password is required and should be minimum 8 symbols');
-//         }
-//         const users = await read();
-//         const newUser = {
-//             id: users.length ? users[users.length - 1].id + 1 : 1,
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: req.body.password
-//         }
-//         users.push(newUser);
-//         await write(users);
-//         res.status(201).json(newUser);
-//     } catch (e) {
-//         res.status(500).json(e.message);
-//     }
-// });
-//
-// app.get('/users/:userId', async (req:Request, res:Response) => {
-//     try {
-//         const users = await read();
-//         const user = users.find(user => user.id === Number(req.params.userId));
-//         res.json(user);
-//     } catch (e) {
-//         res.status(500).json(e.message);
-//     }
-// });
-//
-// app.put('/users/:userId', async (req:Request, res:Response):Promise<void> => {
-//     try {
-//         if (!req.body.name || req.body.name.length < 3) {
-//             return res.status(400).json('Name is required and should be minimum 3 symbols');
-//         }
-//         if (!req.body.email || !req.body.email.includes('@') ) {
-//             return res.status(400).json('Email is required');
-//         }
-//         if (!req.body.password || req.body.password.length < 8) {
-//             return res.status(400).json('Password is required and should be minimum 8 symbols');
-//         }
-//         const users = await read();
-//         const index = users.findIndex(user => user.id === Number(req.params.userId));
-//
-//         if (index === -1) {
-//             return res.status(404).json('User not found');
-//         }
-//         const user = users[index];
-//         user.name = req.body.name;
-//         user.email = req.body.email;
-//         user.password = req.body.password;
-//
-//         await write(users);
-//         res.status(201).json(user);
-//     } catch (e) {
-//         res.status(500).json(e.message);
-//     }
-// })
-//
-// app.delete('/users/:userId', async (req:Request, res:Response):Promise<void> => {
-//     try {
-//         const users = await read();
-//         const index = users.findIndex(user => user.id === Number(req.params.userId));
-//
-//         if (index === -1) {
-//             return res.status(404).json('User not found');
-//         }
-//         users.splice(index, 1);
-//         await write(users);
-//         res.sendStatus(204);
-//     } catch (e) {
-//         res.status(500).json(e.message);
-//     }
-// });
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server has been started on port ${port}`);
 });
