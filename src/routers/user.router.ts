c977b6dccfb0266c7626c07b1ae1a4579b7825e7
Vtorenko25 <oleg.vtorenko@gmail.com>
2024-12-30 17:05:28 +0200

    comit

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,11 +2,16 @@ import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
 import { commonMiddleware } from "../middlewares/common.middleware";
+import { UserValidator } from "../validators/user.validator";
 
 const router = Router();
 
 router.get("/", userController.getList);
-router.post("/", commonMiddleware.validateBody(), userController.create);
+router.post(
+  "/",
+  commonMiddleware.validateBody(UserValidator.create),
+  userController.create,
+);
 
 router.get(
   "/:userId",
@@ -15,9 +20,14 @@ router.get(
 );
 router.put(
   "/:userId",
-  commonMiddleware.validateBody(),
+  commonMiddleware.isIdValid("userId"),
+  commonMiddleware.validateBody(UserValidator.update),
   userController.updateUser,
 );
-router.delete("/:userId", userController.deleteUser);
+router.delete(
+  "/:userId",
+  commonMiddleware.isIdValid("userId"),
+  userController.deleteUser,
+);
 
 export const userRouter = router;
